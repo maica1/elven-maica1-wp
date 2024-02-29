323be5313b56614d916774a0f401b0fd57635a6f
maica1 <maicon.vieira@outlook.com.br>
2024-02-29 01:31:54 -0300

    MOD: Created network infra through module

--- TERRAFORM/main.tf
+++ TERRAFORM/main.tf
@@ -1,4 +1,11 @@
-data "external" "provisioner_public_ip" {
-  program = ["sh", "-c", "curl -s ifconfig.io/ip"]
-}
+# module "s3_backend" {
+#   source = "./MODULES/s3"
 
+# }
+
+module "main_vpc" {
+  source = "./MODULES/network"
+}
+# data "external" "provisioner_public_ip" {
+#   program = ["sh", "-c", "curl -s ifconfig.io/ip"]
+# }
